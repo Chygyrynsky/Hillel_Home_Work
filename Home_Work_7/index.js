84e94d8a78eca55e2e36b974285da72b1a7d3907
Andriy Chygyrynsky <chygyrynskiya@gmail.com>
2023-02-05 17:56:46 +0200

    home work 7  commit

---
+++ Home_Work_7/index.js
@@ -0,0 +1,9 @@
+let number_1 = prompt ('Введіть Перше число: ');
+number_1 = Number(number_1)
+let number_2 = prompt ('Введіть Друге число: ');
+number_2 = Number(number_2)
+let number_3 = prompt ('Введіть Третє число: ');
+number_3 = Number(number_3)
+let result_1 = number_1 + number_2 + number_3;
+let result_2 = result_1 / 3
+alert( ` Cереднє арифметичне трьох Введених чисел :  ${result_2} `);
