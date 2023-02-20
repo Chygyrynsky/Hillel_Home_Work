587f0559e97798f6d632f7f58c43db677f3c25c8
Andriy Chygyrynsky <chygyrynskiya@gmail.com>
2023-02-20 14:19:17 +0200

    home work 15  commit

--- Home_Work_15/index.js
+++ Home_Work_15/index.js
@@ -1,8 +1,10 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
-let indexNumber = prompt(`Введіть індекс елемента, який хочете видалити: ${array}`)
+const array = [" Orange", " Apple", " Cheryy", " Strawberry", " Blueberry", " Pumpkin", " Watermelon"];
+alert(`Масив елементiв :${array}`);
+let indexNumber = prompt(`Введіть індекс елемента, який хочете видалити: 0,1,2,3,4,5,6 ?`)
 
 function removeElement(indexNumber) {
-  delete array[indexNumber];
+ 
+  array.splice(indexNumber, 1);
 }
 removeElement(indexNumber);
 alert(array);
