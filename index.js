c9717e21990e350ea44d4a34d9147b04792b48ba
Chygyrynsky <124130233+Chygyrynsky@users.noreply.github.com>
2023-03-09 15:30:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+window.onload = function(){
+   
+document.getElementById("text").onfocus = function  ()  { 
+    let div = document.crealement('div');
+    document.body.append(div);
+}
+
+document.getElementById("text").onblur = function ()  { 
+    const square = document.querySelector("div");
+    square.remove();
+}
+
+}
+
+
+
+
