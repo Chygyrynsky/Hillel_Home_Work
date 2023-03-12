d72771a43716d2b9f9e9c400848bc34b9dae4adf
Andriy Chygyrynsky <chygyrynskiya@gmail.com>
2023-03-12 17:31:33 +0200

    home work 29

--- Home_Work_29/index.js
+++ Home_Work_29/index.js
@@ -1,36 +1,16 @@
-// function onLoadFunct(){
-
-// document.querySelector("btnPrompt").addEventListener('click', prompt)
-
-// function prompt () {
-//     link = prompt("Введіть посилання:");
-//     if (link.startsWith('http') === false && link.startsWith('https') === false) {
-//         link = `https://${link}`;
-//     }
-// }
-
-// document.querySelector("#btnLink").addEventListener("click", transferLink)
-
-// function transferLink () {
-//         location.href = link;
-// }
-// }
-let btnGetLink = document.querySelector("#btnPrompt");
-let btnGoTolLink = document.querySelector("#btnLink");
-let link;
-
-btnGetLink.addEventListener('click', function () {
-    link = prompt("Введите ссылку");
-    if (link.startsWith('http') === false && link.startsWith('https') === false) {
-        link = `https://${link}`;
+document.querySelector("#buttonLink").addEventListener('click', promptME)
+    
+    function promptME () {
+        link = prompt("Введіть посилання:");
+        if (link.startsWith('http') === false && link.startsWith('https') === false) {
+            link = `https://${link}`;
+        }
     }
-})
-
-
-btnGoTolLink.addEventListener("click", function () {
-    if (!link) {
-        alert('Нет ссылки');
-    } else {
-        location.href = link;
+    
+    document.querySelector("#transferLink").addEventListener("click", transferLink)
+    
+    function transferLink () {
+            location.href = link;
     }
-})
+    
+    
